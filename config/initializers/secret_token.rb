ca0433ddf72364b0134eae838fea7c490b015dc3
Robert Szczerbak <roszczer@gmail.com>
2012-07-04 12:46:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depotpg::Application.config.secret_token = '95e6718ff7bb99f963ec3bb82f99ee738afde0840ca83769f2c860048677a8a030faa973eabce244a7cf63bd1577f8048ca82ab6fa35291937ef5008780d6455'
